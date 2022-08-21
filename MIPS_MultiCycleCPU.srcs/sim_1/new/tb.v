f897d02b630e7843d2fbf8bef7f54266e4da79fe
wh1sperC <1052046259@qq.com>
2022-08-22 01:32:54 +0800

    666

--- MIPS_MultiCycleCPU.srcs/sim_1/new/tb.v
+++ MIPS_MultiCycleCPU.srcs/sim_1/new/tb.v
@@ -30,7 +30,7 @@ module tb(
 wire [31:0] a,b,alu,adr,tom,fromm,pc,ir;
 wire [2:0] state;
 initial begin clock=1;resetn=0;mem_clk=0;
-#0 $display("time\tclock\tresetn\tq\tir\tpc\ta\tb\talu\tfromem\ttomem");
+// #0 $display("time\tclock\tresetn\tq\tir\tpc\ta\tb\talu\tfromem\ttomem");
 #1 resetn=1;
 end
 always #2 mem_clk=~mem_clk;
@@ -38,12 +38,12 @@ always #2 clock=~clock;
 
 
 mccomp m(clock,resetn,state,a,b,alu,adr,tom,fromm,pc,ir,mem_clk); 
-initial begin
-$dumpfile("test.vcd"); 
-$dumpvars;
-$monitor("%g\t %b %b %b %b %b %b %b %b %b %b %b %b",$time,clock,resetn,state,a,b,alu,adr,
-tom,fromm,pc,ir,mem_clk);
-#1200 $finish;
-end
+// initial begin
+// $dumpfile("test.vcd"); 
+// $dumpvars;
+// $monitor("%g\t %b %b %b %b %b %b %b %b %b %b %b %b",$time,clock,resetn,state,a,b,alu,adr,
+// tom,fromm,pc,ir,mem_clk);
+// #1200 $finish;
+// end
 
 endmodule
